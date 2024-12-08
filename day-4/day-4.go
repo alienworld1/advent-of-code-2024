0391a1ea8356839308a36b52a42981cda78308f2
alienworld1 <69631697+alienworld1@users.noreply.github.com>
2024-12-08 10:40:00 +0000

    day 4 part 2

--- day-4/day-4.go
+++ day-4/day-4.go
@@ -7,9 +7,20 @@ import (
 	"os"
 )
 
+func is_valid_position(lines []string, row, col int) bool {
+	return row >= 0 && row < len(lines) && col >= 0 && col < len(lines[0])
+}
+
+func check(lines []string, row, col int, char byte) bool {
+	if !is_valid_position(lines, row, col) {
+		return false
+	}
+	return lines[row][col] == char
+}
+
 func check_direction(lines []string, row, col, dx, dy int, word string) bool {
 	for i := 0; i < len(word); i++ {
-		if row < 0 || row >= len(lines) || col < 0 || col >= len(lines[0]) {
+		if !is_valid_position(lines, row, col) {
 			return false
 		}
 		if lines[row][col] != word[i] {
@@ -61,6 +72,33 @@ func main() {
 		}
 	}
 
-	fmt.Printf("Number of matches: %d\n", num_matches)
+	fmt.Printf("Number of XMAS matches: %d\n", num_matches)
 
+	// Part 2
+
+	num_x_matches := 0
+
+	for i := 0; i < number_of_rows; i++ {
+		for j := 0; j < line_length; j++ {
+			if lines[i][j] == 'M' {
+				if check(lines, i+1, j+1, 'A') {
+					if check(lines, i+2, j+2, 'S') {
+						if (check(lines, i+2, j, 'M') && check(lines, i, j+2, 'S')) || (check(lines, i+2, j, 'S') && check(lines, i, j+2, 'M')) {
+							num_x_matches++
+						}
+					}
+				}
+			} else if lines[i][j] == 'S' {
+				if check(lines, i+1, j+1, 'A') {
+					if check(lines, i+2, j+2, 'M') {
+						if (check(lines, i+2, j, 'M') && check(lines, i, j+2, 'S')) || (check(lines, i+2, j, 'S') && check(lines, i, j+2, 'M')) {
+							num_x_matches++
+						}
+					}
+				}
+			}
+		}
+	}
+
+	fmt.Printf("Number of X-MAS matches: %d\n", num_x_matches)
 }
